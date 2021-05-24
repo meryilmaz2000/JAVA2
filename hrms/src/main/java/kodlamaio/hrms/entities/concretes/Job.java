006c249f00729b65095b6a9d00467dc3cb190edf
Melikşah Eryılmaz <78104557+meryilmaz2000@users.noreply.github.com>
2021-05-24 23:24:32 +0300

    hrms

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Job.java
@@ -0,0 +1,26 @@
+package kodlamaio.hrms.entities.concretes;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="jobs")
+@Data
+public class Job {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="job_title")
+	private String jobTitle;
+	
+}
